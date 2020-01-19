53d700975bd19c6fc44b43bf8ef0c649f7fcef9d
vu <anh-vu.nguyen@telecom-paris.fr>
2020-01-19 18:15:21 +0100

    created usefulclasses packege with Communicator,Event, Notification, POI, ProfilComplete, and ProfilSmall classes

---
+++ Android/Green Guy/app/src/main/java/usefulclasses/ProfilSmall.java
@@ -0,0 +1,69 @@
+package usefulclasses;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class ProfilSmall {
+    private long id;
+    private String realName;
+    private String pseudo;
+    private String description;
+    private String mail;
+    private LatLng lastPosition=new LatLng(0,0);
+    public ProfilSmall (String realName,String pseudo,String description,String mail,LatLng lastPosition,long id){
+        this.realName=realName;
+        this.pseudo=pseudo;
+        this.description=description;
+        this.mail=mail;
+        this.lastPosition=lastPosition;
+        this.id=id;
+    }
+
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public LatLng getLastPosition() {
+        return lastPosition;
+    }
+
+    public void setLastPosition(LatLng lastPosition) {
+        this.lastPosition = lastPosition;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
