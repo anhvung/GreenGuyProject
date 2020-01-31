b0cb2b8d0657266c654a4fa9ca7584018fff5775
Anh-Vu Nguyen <anh-vu.nguyen@telecom-paris.fr>
2020-01-31 13:26:40 +0100

    Update Serveur.java

--- Serveur/Serveur.java
+++ Serveur/Serveur.java
@@ -6,7 +6,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 /*
- * www.codeurjava.com
+ *\brief Classe Serveur, il s'agit pour le moment d'un code servant de point de dÃ©part
  */
 public class Serveur {
  
@@ -16,19 +16,19 @@ public class Serveur {
      final Socket clientSocket ;
      final BufferedReader in;
      final PrintWriter out;
-     final Scanner sc=new Scanner(System.in); //pour lire à partir du clavier
+     final Scanner sc=new Scanner(System.in); //pour lire  partir du clavier
   
      try {
-    	//créer un socket serveur avec un numéro de port 5000
+    	//crer un socket serveur avec un numro de port 5000
        serveurSocket = new ServerSocket(5000);
-     //écoute pour une connexion à apporter avec ce socket et l'accepte
+     //coute pour une connexion  apporter avec ce socket et l'accepte
        clientSocket = serveurSocket.accept();
      //flux pour envoyer
        out = new PrintWriter(clientSocket.getOutputStream());
      //flux pour recevoir
        in = new BufferedReader (new InputStreamReader (clientSocket.getInputStream()));
-       /* il faut créer 2 processus (threads parce que l'envoi et la réception se font simulatnément
-    		 cela nous permet d'envoyer et de recevoir des messages en même temps */  
+       /* il faut crer 2 processus (threads parce que l'envoi et la rception se font simulatnment
+    		 cela nous permet d'envoyer et de recevoir des messages en mme temps */  
        Thread envoi= new Thread(new Runnable() {
           String msg;
           @Override
@@ -48,13 +48,13 @@ public class Serveur {
           public void run() {
              try {
                 msg = in.readLine();
-                //tant que le client est connecté
+                //tant que le client est connect
                 while(msg!=null){
                    System.out.println("Client : "+msg);
                    msg = in.readLine();
                 }
-                //sortir de la boucle si le client a déconecté
-                System.out.println("Client déconecté");
+                //sortir de la boucle si le client a dconect
+                System.out.println("Client dconect");
                 //fermer le flux et la session socket
                 out.close();
                 clientSocket.close();
