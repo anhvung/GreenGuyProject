1f88f9822b3e9ae692177c0a4537a35ddf505bab
Dorge <ldorge@telecom-paris.fr>
2020-03-11 23:03:03 +0100

    ajout attributs à la classe Event

--- Android/Green Guy/app/src/main/java/com/pafloca/greenguy/ModelEvent.java
+++ Android/Green Guy/app/src/main/java/com/pafloca/greenguy/ModelEvent.java
@@ -2,6 +2,61 @@ package com.pafloca.greenguy;
 
 public class ModelEvent {
 
+    private String titre;
+    private String soustitre;
+    private String publieur;
+    private String date;
+    private String lieu;
 
+    public ModelEvent(){
 
+    }
+
+    public ModelEvent(String titre, String soustitre, String publieur, String date, String lieu) {
+        this.titre = titre;
+        this.soustitre = soustitre;
+        this.publieur = publieur;
+        this.date = date;
+        this.lieu = lieu;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String getSoustitre() {
+        return soustitre;
+    }
+
+    public void setSoustitre(String soustitre) {
+        this.soustitre = soustitre;
+    }
+
+    public String getPublieur() {
+        return publieur;
+    }
+
+    public void setPublieur(String publieur) {
+        this.publieur = publieur;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
