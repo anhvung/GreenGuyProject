9bed0adda1f02210f16744ead376ce085de7f1d1
vu <anh-vu.nguyen@telecom-paris.fr>
2020-05-11 20:15:41 +0200

    image upload now supported

--- Android/Green Guy/app/src/main/java/com/pafloca/greenguy/MyProfileActivity.java
+++ Android/Green Guy/app/src/main/java/com/pafloca/greenguy/MyProfileActivity.java
@@ -3,15 +3,28 @@ package com.pafloca.greenguy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import usefulclasses.ClientConnexion;
 
+import static com.pafloca.greenguy.IdentificationActivity.sep;
 import static java.lang.Math.min;
 
 public class MyProfileActivity extends AppCompatActivity {
@@ -22,6 +35,9 @@ public class MyProfileActivity extends AppCompatActivity {
     ImageView photo;
     TextView nom;
     TextView lieu;
+    ImageButton setImage;
+    ImageView my_page_profil_image;
+    Bitmap image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,11 +46,41 @@ public class MyProfileActivity extends AppCompatActivity {
         age = findViewById(R.id.age);
         nom = findViewById(R.id.nom);
         lieu = findViewById(R.id.lieu);
+        setImage=findViewById(R.id.setImage);
+        my_page_profil_image=findViewById(R.id.my_page_profil_image);
+        setImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 42);
+            }
+        });
         SharedPreferences sharedPref = getSharedPreferences("SAVE", Context.MODE_PRIVATE);
         storedId = sharedPref.getInt("USER_ID", -1);
         new getInfo().execute();
     }
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
 
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                image=selectedImage;
+                new getPicture().execute();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(MyProfileActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        }else {
+            Toast.makeText(MyProfileActivity.this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        }
+    }
     public void updateInfo(){
         nom.setText(response[0]);
         age.setText(response[1]);
@@ -68,4 +114,31 @@ public class MyProfileActivity extends AppCompatActivity {
             updateInfo();
         }
     }
+    private class getPicture extends AsyncTask<Void,Void,Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            ClientConnexion connect= new ClientConnexion("192.168.1.17",2345,"0005",storedId+sep+bitmapToBase64(image));
+            sendMsg=connect.magicSauce()[0];
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            byte[] decodedString = Base64.decode(sendMsg, Base64.DEFAULT);
+            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+            my_page_profil_image.setImageBitmap(decodedByte);
+        }
+    }
+
+    public String bitmapToBase64(Bitmap im){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        im=Bitmap.createScaledBitmap(im, 500, 500, true);
+        im.compress(Bitmap.CompressFormat.JPEG, 75, baos); // bm is the bitmap object
+        byte[] b = baos.toByteArray();
+        return Base64.encodeToString(b, Base64.DEFAULT);
+    }
+
+
 }
